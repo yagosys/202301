5e2a77f3996d61c3b1ad68581a1d55384e6aaf16
andy <yagosys@gmail.com>
2023-02-06 12:05:38 +0800

    client enviroment for install aws cli and terraform

---
+++ deployment/client_setup_aws_terraform.sh
@@ -0,0 +1,6 @@
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws configure
+aws s3 ls
+sudo snap install terraform --classic
