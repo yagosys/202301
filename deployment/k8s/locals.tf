dd1230a6866442d4b230249d4e15d4f5c165cc5d
andy <yagosys@gmail.com>
2023-02-07 07:43:53 +0800

    add imageid map

---
+++ deployment/k8s/locals.tf
@@ -0,0 +1,23 @@
+locals{
+ec2_image_id_map={
+ap-south-1 : "ami-06984ea821ac0a879"
+eu-north-1 : "ami-00c70b245f5354c0a"
+eu-west-3 : "ami-0afd55c0c8a52973a"
+eu-west-2 : "ami-0d09654d0a20d3ae2"
+eu-west-1 : "ami-0333305f9719618c7"
+ap-northeast-3 : "ami-0ead712799090892e"
+ap-northeast-2 : "ami-09eba584c30b7299f"
+ap-northeast-1 : "ami-0cd7ad8676931d727"
+ca-central-1 : "ami-0dae3a932d090b3de"
+sa-east-1 : "ami-015e30624fffff117"
+ap-east-1 : "ami-0bc44b8dc7cae9c34"
+ap-southeast-1 : "ami-0b7e55206a0a22afc"
+ap-southeast-2 : "ami-0c18f3cdeea1c220d"
+eu-central-1 : "ami-03e08697c325f02ab"
+us-east-1 : "ami-00874d747dde814fa"
+us-east-2 : "ami-0ab0629dba5ae551d"
+us-west-1 : "ami-09b2a1e33ce552e68"
+us-west-2 : "ami-095413544ce52437d"
+}
+}
+
