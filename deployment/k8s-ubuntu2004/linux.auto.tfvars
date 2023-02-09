794ab784ca64b92f026b5fa5b9de04e5b09b23a9
andy <yagosys@gmail.com>
2023-02-09 18:04:41 +0800

    update

---
+++ deployment/k8s-ubuntu2004/linux.auto.tfvars
@@ -0,0 +1,5 @@
+region="ap-east-1"
+instance_type="t3.large" 
+cfosLicense="/home/i/202301/deployment/k8s/fos_license.yaml"
+key_location="~/.ssh/id_rsa"
+dockerinterbeing="/home/i/202301/deployment/private/dockerinterbeing.yaml"
---
+++ deployment/k8s-ubuntu2004/locals.tf
@@ -0,0 +1,22 @@
+locals{
+ec2_image_id_map={
+ap-south-1 = "ami-0597375488017747e"
+eu-north-1 = "ami-061eda1ede961cfb2"
+eu-west-3 = "ami-0a89a7563fc68be84"
+eu-west-2 = "ami-00de6c6491fdd3ef5"
+eu-west-1 = "ami-0c68b55d1c875067e"
+ap-northeast-3 = "ami-0e7a53a420b622cf7"
+ap-northeast-2 = "ami-051d287d02a19035f"
+ap-northeast-1 = "ami-068e3d6bc44010346"
+ca-central-1 = "ami-0b71701f667d37ddf"
+sa-east-1 = "ami-0dce57edaf582cf62"
+ap-east-1 = "ami-069b805a94079552a"
+ap-southeast-1 = "ami-08be951cec06726be"
+ap-southeast-2 = "ami-0e2e4c9e55712f9e3"
+eu-central-1 = "ami-076bdd070268f9b8d"
+us-east-1 = "ami-0778521d914d23bc1"
+us-east-2 = "ami-00149760ce42c967b"
+us-west-1 = "ami-03e0029f86a2c74c3"
+us-west-2 = "ami-04bad3c587fe60d89"
+}
+}
