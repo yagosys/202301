dc78294233d187631bc86f377670bac0db4cbc94
andy <yagosys@gmail.com>
2023-02-10 18:09:53 +0800

    add north non vrf

--- north/deploy.sh
+++ north/deploy.sh
@@ -2,5 +2,5 @@
    kubectl apply -f 1_net_attach_10_1_128_ipam_whereabouts.yaml
    kubectl apply -f br-10-1-128-1-static-for_cfos.yaml
    kubectl apply -f 3_cfosdeployment.yaml
-   kubectl apply -f deployment_tool_net1_vrf.yaml
+   kubectl apply -f deployment_tool_net1.yaml
    kubectl apply -f firewallpolicy.yaml
