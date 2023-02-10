bf098cd78c90af13068ea59a05bd33da06d03c82
andy <yagosys@gmail.com>
2023-02-10 15:39:12 +0800

    north-bound egress traffic use case

--- north/deploy.sh
+++ north/deploy.sh
@@ -1,7 +1,5 @@
    kubectl apply -f 0_pv_pvc_role.yaml
-   kubectl apply -f 1_net_attach_10_1_128.yaml
-   kubectl apply -f 2_net_attach_10_2_128.yaml
+   kubectl apply -f 1_net_attach_10_1_128_ipam_whereabouts.yaml
    kubectl apply -f br-10-1-128-1-static-for_cfos.yaml
-   kubectl apply -f br-10-2-128-1-static-for-cfos.yaml
    kubectl apply -f 3_cfosdeployment.yaml
    kubectl apply -f deployment_tool_net1_vrf.yaml
---
+++ north/undeploy.sh
@@ -0,0 +1,5 @@
+
+   kubectl delete -f deployment_tool_net1_vrf.yaml
+   kubectl delete -f 3_cfosdeployment.yaml
+   kubectl delete -f br-10-1-128-1-static-for_cfos.yaml
+   kubectl delete -f 1_net_attach_10_1_128_ipam_whereabouts.yaml
