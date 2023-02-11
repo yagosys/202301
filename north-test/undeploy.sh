6306f19966413a37c792faf295539e968188dd9a
andy <yagosys@gmail.com>
2023-02-11 17:36:19 +0800

    update

---
+++ north-test/undeploy.sh
@@ -0,0 +1,8 @@
+sudo cp ./misc/100-crio-bridge.conf.ori /etc/cni/net.d/100-crio-bridge.conf
+kubectl rollout restart daemonset kube-multus-ds -n kube-system
+kubectl delete -f firewallpolicy.yaml
+kubectl delete -f 1_net_attach_10_1_128_ipam_whereabouts.yaml
+kubectl delete -f br-10-1-128-1-static-for_cfos.yaml
+kubectl delete -f 3_cfosdeployment.yaml
+kubectl delete -f deployment_tool_net1.yaml
+kubectl delete -f 0_pv_pvc_role.yaml
