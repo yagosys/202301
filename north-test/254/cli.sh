d37eb61835819cd376242726fa311a5925bda949
andy <yagosys@gmail.com>
2023-02-14 16:53:42 +0800

    add scale out cfos

--- north-test/254/cli.sh
+++ north-test/254/cli.sh
@@ -1,9 +1,9 @@
 echo 'cfos interface address'
-cfospod=`kubectl get pod | grep fos | grep -v 254 | cut -d ' ' -f 1 | tail -n -1`
+cfospod=`kubectl get pod | grep fos.*254 | cut -d ' ' -f 1 | tail -n -1`
 kubectl exec -it po/$cfospod -- ip  -4 -p -h -br a
 
 echo 'multitool pod interface addaddress and default route'
-multpod=`kubectl get pod | grep mult | grep -v 254 | cut -d ' ' -f 1 | tail -n -1`
+multpod=`kubectl get pod | grep mult.*254 | cut -d ' ' -f 1 | tail -n -1`
 kubectl exec -it po/$multpod -- ip  -4 -p -h -br a
 kubectl exec -it po/$multpod -- ip -4 -p -h -br  route | grep default
 
@@ -13,7 +13,7 @@ kubectl exec -it po/$multpod -- ping -c 2 -I net1 1.1.1.1
 echo 'issue normal curl'
 kubectl exec -it po/$multpod -- curl -k -I https://1.1.1.1
 
-echo 'issue curl with attack '
+echo 'issue curl with attach '
 sleep 1
 kubectl exec -it po/$multpod --  curl --max-time 5  -k -H "User-Agent: () { :; }; /bin/ls" https://1.1.1.1
 
