cfe1e10abfaec828960ba763aae90b827a6b8bb0
andy <yagosys@gmail.com>
2023-02-18 16:43:05 +0800

    updated

--- north-test/new/cli01.sh
+++ north-test/new/cli01.sh
@@ -20,3 +20,6 @@ kubectl exec -it po/$multpod --  curl --max-time 5  -k -H "User-Agent: () { :; }
 echo 'check attak log on cfos'
 sleep 1
 kubectl exec -it po/$cfospod -- tail -n 1 /data/var/log/log/ips.0
+
+kubectl get pod | grep multi | awk '{print $1}'  | while read line; do kubectl exec -t po/$line -- curl -k -I  https://www.eicar.org/download/eicar.com.txt ; done
+
