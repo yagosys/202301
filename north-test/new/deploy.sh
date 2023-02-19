5ed29a8ce47003e488f0a7c97a025ce1e7fa1ceb
andy <yagosys@gmail.com>
2023-02-19 15:46:34 +0800

    deploy.sh

---
+++ north-test/new/deploy.sh
@@ -0,0 +1,10 @@
+kubectl apply -f 00_net-10-1-128.yaml
+kubectl apply -f 00_pv_pvc.yaml
+kubectl apply -f 01_cfos_account.yaml
+kubectl apply -f 02_cfos01.yaml
+kubectl apply -f 03_cfos_defaultroute.yaml
+kubectl apply -f 04_cfosfirewallpolicy.yaml
+kubectl apply -f 05_cfos_dns.yaml
+kubectl apply -f 05_multtool_with_defaultroute01.yaml
+cfospod=`kubectl get pod | grep fos01 | grep -v terminat | cut -d ' ' -f 1 | tail -n -1`
+kubectl delete po/$cfospod
