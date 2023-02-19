0b4c1af903c1794a9f6ad09c522b072d1434635e
andy <yagosys@gmail.com>
2023-02-19 15:17:40 +0800

    update

--- north-test/new/updatedefaultcniconfig.sh
+++ north-test/new/updatedefaultcniconfig.sh
@@ -4,14 +4,21 @@ function waitforMultusReady {
                echo "multus daemonSet Ready"
                break
              else
-               echo "multus daemonSet has not ready yet"
+               echo "wait for multus daemonSet ready "
                sleep 1
              fi
         done
 }
 
+
 dnsserver=$(grep nameserver  /run/systemd/resolve/resolv.conf | awk  '{print $2}')
 sudo sed -i 's/"dst": "0.0.0.0\/0"/"dst": "10.96.0.0\/12", "gw": "10.85.0.1"},\n            { "dst": "'"$dnsserver"'\/32", "gw": "10.85.0.1"/g' /etc/cni/net.d/100-crio-bridge.conf
 kubectl rollout restart ds/kube-multus-ds -n kube-system
 waitforMultusReady
-sudo tail -f /etc/cni/net.d/00-multus.conf --retry | sed '/10.96/q'
+while true; do
+  if sudo grep -q "10.96" /etc/cni/net.d/00-multus.conf; then
+    echo "Done!"
+    break
+  fi
+  sleep 1
+done
