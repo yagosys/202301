afe98be9b93275f3be6270b6a3071a91d0dfe0aa
andy <yagosys@gmail.com>
2023-02-20 14:24:02 +0800

    fix policy not apply issue

--- north-test/2023220/cfos_demo_bridge_egress/deploy.sh
+++ north-test/2023220/cfos_demo_bridge_egress/deploy.sh
@@ -7,4 +7,19 @@ kubectl create -f 04_cfosfirewallpolicy.yaml
 kubectl create -f 05_cfos_dns.yaml
 kubectl create -f cfosdeployment.yaml
 kubectl create -f application.yaml
+
+function  deploymentReady() {
+while true; do
+  output=$(kubectl get pod)
+  if echo "$output" | grep -q "fos-deployment.*1/1" && echo "$output" | grep -q "multitool01-deployment.*1/1"; then
+    echo "Both pods are ready!"
+    break
+  fi
+  sleep 5
+done
+}
+
+deploymentReady
+kubectl rollout restart deployment fos-deployment
+deploymentReady
 #after deployment, the cfos may not work. run checkcfosreadiness.sh to fix it
