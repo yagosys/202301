f20b7ad478a6e633c9e08905c5c7546bbea732e8
andy <yagosys@gmail.com>
2023-02-20 16:44:32 +0800

    update

---
+++ north-test/2023220/cfos_demo_bridge_egress/deploy1.sh
@@ -0,0 +1,78 @@
+kubectl create -f 00_pv_pvc.yaml
+kubectl create -f /home/ubuntu/fos_license.yaml
+kubectl create -f net_10_1_128.yaml
+kubectl create -f net_default_10_86_0.yaml
+kubectl create -f 01_cfos_account.yaml
+kubectl create -f cfosdeployment.yaml
+kubectl create -f 03_cfos_defaultroute.yaml
+kubectl create -f 04_cfosfirewallpolicy.yaml
+kubectl create -f 05_cfos_dns.yaml
+kubectl create -f application.yaml
+
+sleep 30
+function  deploymentReady() {
+while true; do
+  output=$(kubectl get pod)
+  if echo "$output" | grep -q "fos-deployment.*1/1" && echo "$output" | grep -q "multitool01-deployment.*1/1"; then
+    echo "Both pods are ready!"
+    break
+  fi
+    sleep 5
+done
+
+}
+
+deploymentReady
+
+function cfos_ping_1_1_1_1() {
+
+cfospod=`kubectl get pod | grep fos | grep -v terminat | cut -d ' ' -f 1 | tail -n -1`
+multpod=`kubectl get pod | grep multitool01 |  grep -v terminat |  cut -d ' ' -f 1 | tail -n -1`
+
+kubectl exec -it po/$cfospod -- ping -c 1 1.1.1.1 > /dev/null 2>&1
+
+   if [ $? -eq 0 ]; then
+     return 0
+   else
+     return 1
+   fi
+}
+
+function ping_1_1_1_1() {
+
+cfospod=`kubectl get pod | grep fos | grep -v terminat | cut -d ' ' -f 1 | tail -n -1`
+multpod=`kubectl get pod | grep multitool01 |  grep -v terminat |  cut -d ' ' -f 1 | tail -n -1`
+
+kubectl exec -it po/$multpod -- ping -c 1 1.1.1.1 > /dev/null 2>&1
+
+   if [ $? -eq 0 ]; then
+     echo ping 1.1.1.1 is ok
+     return 0
+   else
+     echo ping 1.1.1.1 is not reachable
+     return 1
+   fi
+}
+
+   cfos_ping_1_1_1_1
+   result=$?
+   if [ $result -eq 0 ] ; then
+        echo cfos is able to reach 1.1.1.1
+   else
+        echo cfos is not reach 1.1.1.1, something wrong
+	exit
+   fi
+
+   ping_1_1_1_1
+   result=$?
+   if [ $result -eq 0 ] ; then
+        echo application pod  is able to reach 1.1.1.1
+   else
+        echo application pod  is not reach 1.1.1.1, restart cfos
+        kubectl rollout restart deployment fos-deployment
+        sleep 10
+        deploymentReady
+	ping_1_1_1_1
+   fi
+
+#after deployment, the cfos may not work. run checkcfosreadiness.sh to fix it
