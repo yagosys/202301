5e09454346e92c64dd22c2addcc7bdae38e09760
andy <yagosys@gmail.com>
2023-02-22 08:50:31 +0800

    ssh.sh

---
+++ deployment/k8s/ssh.sh
@@ -0,0 +1,4 @@
+$ip="54.198.18.106"
+scp -i ~/.ssh/id_ed25519cfoslab /home/i/.ssh/id_ed25519cfoslab ubuntu@$ip:.ssh/id_ed25519cfoslab
+scp -i ~/.ssh/id_ed25519cfoslab /home/i/.ssh/id_ed25519cfoslab.pub ubuntu@$ip:.ssh/id_ed25519cfoslab.pub
+
