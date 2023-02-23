18f2b7107a496df97cc5a00c284bb740c6d02176
andy <yagosys@gmail.com>
2023-02-23 10:12:27 +0800

    remove defaultroute

--- north-test/2023220/flannel/deploy.sh
+++ north-test/2023220/flannel/deploy.sh
@@ -1,6 +1,5 @@
 kubectl create -f 00_pv_pvc.yaml
 kubectl create -f 01_cfos_account.yaml
-kubectl create -f 03_cfos_defaultroute.yaml
 kubectl create -f 04_cfosfirewallpolicy.yaml
 kubectl create -f 05_cfos_dns.yaml
 kubectl create -f net_bridge_secondary_network.yaml
