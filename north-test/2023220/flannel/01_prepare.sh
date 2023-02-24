03ae5904d549aad9f00fe6d4f284a7e333425040
andy <yagosys@gmail.com>
2023-02-24 10:26:56 +0800

    update

--- north-test/2023220/flannel/01_prepare.sh
+++ north-test/2023220/flannel/01_prepare.sh
@@ -1,6 +1,6 @@
-scp  -i ~/.ssh/id_ed25519cfoslab ./createcniconfig.sh 10.0.2.200:
-scp  -i ~/.ssh/id_ed25519cfoslab ./createcniconfig.sh 10.0.2.201:
-ssh -i ~/.ssh/id_ed25519cfoslab 10.0.2.200  ./createcniconfig.sh
-ssh -i ~/.ssh/id_ed25519cfoslab 10.0.2.201  ./createcniconfig.sh
+scp  -i ~/.ssh/id_ed25519cfoslab ./02_createcniconfig.sh 10.0.2.200:
+scp  -i ~/.ssh/id_ed25519cfoslab ./02_createcniconfig.sh 10.0.2.201:
+ssh -i ~/.ssh/id_ed25519cfoslab 10.0.2.200  ./02_createcniconfig.sh
+ssh -i ~/.ssh/id_ed25519cfoslab 10.0.2.201  ./02_createcniconfig.sh
 kubectl label nodes ip-10-0-2-200   kubernetes.io/role=worker --overwrite
 kubectl label nodes ip-10-0-2-201   kubernetes.io/role=worker --overwrite
