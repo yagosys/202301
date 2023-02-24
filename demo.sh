6458015ddcc5e42f9d24e965a5dfc1f2b609dade
andy <yagosys@gmail.com>
2023-02-24 16:23:17 +0800

    test

--- demo.sh
+++ demo.sh
@@ -1,7 +1,7 @@
-cd /Users/i/202301/north-test/2023220/flannel
+cd /home/ubuntu/202301/north-test/2023220/flannel
 kubectl label nodes ip-10-0-2-200   kubernetes.io/role=worker --overwrite
 kubectl label nodes ip-10-0-2-201   kubernetes.io/role=worker --overwrite
-/Users/i/202301/north-test/2023220/flannel/03_deploy.sh
+/home/ubuntu/202301/north-test/2023220/flannel/03_deploy.sh
 kubectl rollout status ds/fos-deployment
 kubectl create -f app.yaml
 
