89348d08c41129516d0fef7a9ac38a0604cdc252
andy <yagosys@gmail.com>
2023-02-24 11:07:04 +0800

    test

--- north-test/2023220/flannel/04_restart_cfos_ds.sh
+++ north-test/2023220/flannel/04_restart_cfos_ds.sh
@@ -4,7 +4,6 @@ for node in $workernodes
 do
 cfospod=`kubectl get pod -l app=fos --field-selector spec.nodeName=$node | grep fos | grep -v terminat | cut -d ' ' -f 1 | tail -n -1`
 multpod=`kubectl get pod -l app=multitool01 --field-selector spec.nodeName=$node | grep multitool01 |  grep -v terminat |  cut -d ' ' -f 1 | tail -n -1`
-echo $node
 kubectl exec -it po/$cfospod -- ping -c 1 1.1.1.1 > /dev/null 2>&1
 
    if [ $? -eq 0 ]; then
@@ -12,9 +11,27 @@ kubectl exec -it po/$cfospod -- ping -c 1 1.1.1.1 > /dev/null 2>&1
      continue
    else
      cfos is not able to reach 1.1.1.1, something wrong
-     kubectl rollout restart ds fos-deployment
    fi
 done
 }
 
+function app_ping_1_1_1_1() {
+workernodes=$(kubectl get nodes -l 'kubernetes.io/role=worker' -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
+for node in $workernodes
+do
+cfospod=`kubectl get pod -l app=fos --field-selector spec.nodeName=$node | grep fos | grep -v terminat | cut -d ' ' -f 1 | tail -n -1`
+multpod=`kubectl get pod -l app=multitool01 --field-selector spec.nodeName=$node | grep multitool01 |  grep -v terminat |  cut -d ' ' -f 1 | tail -n -1`
+kubectl exec -it po/$multpod -- ping -c 1 1.1.1.1 > /dev/null 2>&1
+
+   if [ $? -eq 0 ]; then
+    echo $node/$cfospod , $multpod is able to reach 1.1.1.1
+     continue
+   else
+    echo $node/$multpod is not able to reach 1.1.1.1, something wrong
+    echo restart cfos ds
+     kubectl rollout restart ds fos-deployment
+   fi
+done
+}
 cfos_ping_1_1_1_1
+app_ping_1_1_1_1
