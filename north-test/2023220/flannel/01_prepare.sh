072385321ce65f756e563f413b0609710be4fe97
andy <yagosys@gmail.com>
2023-02-24 11:30:12 +0800

    test

--- north-test/2023220/flannel/01_prepare.sh
+++ north-test/2023220/flannel/01_prepare.sh
@@ -1,5 +1,6 @@
-scp  -i ~/.ssh/id_ed25519cfoslab ./02_createcniconfig.sh 10.0.2.200:
-scp  -i ~/.ssh/id_ed25519cfoslab ./02_createcniconfig.sh 10.0.2.201:
+cd /home/ubuntu/202301/north-test/2023220/flannel
+scp  -i ~/.ssh/id_ed25519cfoslab -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ./02_createcniconfig.sh 10.0.2.200:
+scp  -i ~/.ssh/id_ed25519cfoslab -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ./02_createcniconfig.sh 10.0.2.201:
 ssh -i ~/.ssh/id_ed25519cfoslab 10.0.2.200  ./02_createcniconfig.sh
 ssh -i ~/.ssh/id_ed25519cfoslab 10.0.2.201  ./02_createcniconfig.sh
 kubectl label nodes ip-10-0-2-200   kubernetes.io/role=worker --overwrite
