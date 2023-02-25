20508ffd4cda39a3b7969fd75a6cd3903445cd20
andy <yagosys@gmail.com>
2023-02-25 15:49:06 +0800

    test

--- north-test/2023220/flannel/undeploy.sh
+++ north-test/2023220/flannel/undeploy.sh
@@ -3,5 +3,3 @@ kubectl delete -f net_bridge_secondary_network.yaml
 kubectl delete -f net_new_default_network_for_cfos.yaml
 kubectl delete -f cfosdeployment.yaml
 kubectl delete -f app.yaml
-ssh -i ~/.ssh/id_ed25519cfoslab 10.0.2.200 sudo ip  link delete cni5
-ssh -i ~/.ssh/id_ed25519cfoslab 10.0.2.201 sudo ip  link delete cni5
