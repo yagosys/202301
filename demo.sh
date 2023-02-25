490a457643b09f5c11f783e3de0db7541c8c05fc
andy <yagosys@gmail.com>
2023-02-25 12:59:59 +0800

    test

--- demo.sh
+++ demo.sh
@@ -1,6 +1,5 @@
 cd /home/ubuntu/202301/north-test/2023220/flannel
-kubectl label nodes ip-10-0-2-200   kubernetes.io/role=worker --overwrite
-kubectl label nodes ip-10-0-2-201   kubernetes.io/role=worker --overwrite
+kubectl get node  |  grep ip- | awk '{print $1}' |  while read line; do kubectl label nodes $line   kubernetes.io/role=worker --overwrite  ; done
 /home/ubuntu/202301/north-test/2023220/flannel/03_deploy.sh
 kubectl rollout status ds/fos-deployment
 kubectl rollout restart ds/fos-deployment
