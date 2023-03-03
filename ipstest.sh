1a385a030a64c7694fc417da9b1aa42f8959288e
andy <yagosys@gmail.com>
2023-03-03 16:59:55 +0800

    exclude terminiated or terminating pod

--- ipstest.sh
+++ ipstest.sh
@@ -1,1 +1,1 @@
-kubectl get pod | grep multi | awk '{print $1}'  | while read line; do kubectl exec -t po/$line --  curl --max-time 5  -k -H "User-Agent: () { :; }; /bin/ls" https://1.1.1.1  ; done
+kubectl get pod | grep multi | grep -v termin | awk '{print $1}'  | while read line; do kubectl exec -t po/$line --  curl --max-time 5  -k -H "User-Agent: () { :; }; /bin/ls" https://1.1.1.1  ; done
