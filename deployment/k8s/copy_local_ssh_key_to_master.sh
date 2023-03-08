a88c01bea455b4bb55fb719d70c769a430c3cec8
andy <yagosys@gmail.com>
2023-03-08 14:35:49 +0800

    test

--- deployment/k8s/copy_local_ssh_key_to_master.sh
+++ deployment/k8s/copy_local_ssh_key_to_master.sh
@@ -1,4 +1,4 @@
 ip=$(terraform output --raw instance_public_ip)
 scp -i ~/.ssh/id_ed25519cfoslab /home/i/.ssh/id_ed25519cfoslab ubuntu@$ip:.ssh/id_ed25519cfoslab
 scp -i ~/.ssh/id_ed25519cfoslab /home/i/.ssh/id_ed25519cfoslab.pub ubuntu@$ip:.ssh/id_ed25519cfoslab.pub
-ssh -i ~/.ssh/id_ed25519cfoslab -l ubuntu $ip /home/ubuntu/202301/demo.sh
+ssh -i ~/.ssh/id_ed25519cfoslab -l ubuntu $ip /home/ubuntu/202301/demo_default.sh
