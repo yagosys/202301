b4e4829e708db3ebc3ad1139a386b66d076b0d25
andy <yagosys@gmail.com>
2023-03-13 09:24:24 +0800

    calico multinode cfos

---
+++ north-test/2023220/calico/03_deploy.sh
@@ -0,0 +1,7 @@
+kubectl create -f 00_pv_pvc.yaml
+kubectl create -f 01_cfos_account.yaml
+kubectl create -f 04_cfosfirewallpolicy.yaml
+kubectl create -f 05_cfos_dns.yaml
+kubectl create -f  net_bridge_secondary_network_full.yaml
+echo deploy cfos daemonSet
+kubectl create -f cfosdeployment.yaml
