934c801034c9fc3a3dd8353664b20e938e93ac95
andy <yagosys@gmail.com>
2023-03-16 09:06:44 +0800

    delete

--- north-default/cli.sh
+++
@@ -1,5 +0,0 @@
-pod=`kubectl get pod | grep multi | cut -d ' ' -f 1 | tail -n -1`
-kubectl exec -it po/$pod --  curl -I -k https://1.1.1.1
-kubectl exec -it po/$pod --  curl -k -H "User-Agent: () { :; }; /bin/ls" https://1.1.1.1
-cfospod=`kubectl get pod | grep fos | cut -d ' ' -f 1 | tail -n -1`
-kubectl exec -it po/$cfospod -- tail -f /data/var/log/log/ips.0
