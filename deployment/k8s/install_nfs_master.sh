78248d215270e5917d3a9c508062d234dfd35001
andy <yagosys@gmail.com>
2023-03-16 09:47:21 +0800

    delete

--- deployment/k8s/install_nfs_master.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y nfs-server
-sudo -u ubuntu  mkdir /home/ubuntu/data
-cat << EOF | sudo   tee  /etc/exports
-/home/ubuntu/data 10.0.1.100(rw,no_subtree_check,no_root_squash)
-EOF
-sudo systemctl enable --now nfs-server
-sudo exportfs -ar
