cc98d72ce43934971738c87a9460fa3ce124b19d
andy <yagosys@gmail.com>
2023-03-20 19:44:00 +0800

    demo cfos on eks

--- demo/eks/demo.sh
+++ demo/eks/demo.sh
@@ -20,9 +20,11 @@ function restart_cfos_if_not_ready {
 kubectl create -f multus-daemonset.yml
 kubectl create -f nad_bridge_cni_10_0_200_cfosdefauultcni5.yaml
 kubectl rollout status ds/kube-multus-ds -n kube-system
+sleep 10
 kubectl create -f dockersecret.yaml
 kubectl create -f fos_license.yaml
 kubectl create -f cfos_firewallpolicy.yaml
+sleep 10
 kubectl create -f app.yaml
 kubectl rollout status deployment
 kubectl create -f cfos_account.yaml
