a447c5621d55dad9f7a531ed57f535b329dbdd38
andy <yagosys@gmail.com>
2023-03-21 16:49:16 +0800

    test

--- demo/tools/verifyping.sh
+++ demo/tools/verifyping.sh
@@ -8,6 +8,7 @@ for nodeName in $node_list; do
         kubectl rollout status deployment multitool01-deployment
         cfospod=`kubectl get pods -l app=fos --field-selector spec.nodeName=$nodeName |    cut -d ' ' -f 1 | tail -n -1`
         multpod=`kubectl get pods -l app=multitool01 --field-selector spec.nodeName=$nodeName |   cut -d ' ' -f 1 | tail -n -1`
+	iplist=`kubectl get pods -l app=multitool01 -o=jsonpath='{range .items[*]}{.status.podIP}{"\n"}{end}'`
         if  kubectl exec -it po/$multpod -- ping -c 1 1.1.1.1
         then
           echo " on $nodeName cfos is ready"
@@ -15,4 +16,9 @@ for nodeName in $node_list; do
           echo "  "
 
         fi
+	for ip in $iplist; do 
+	  echo ping from $multpod to $ip
+	  kubectl exec -it po/$multpod -- ping -c 1 $ip
+       done
+       
 done
