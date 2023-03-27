f96c5364f7152d435dc8fe38e0f992eee66a3992
andy <yagosys@gmail.com>
2023-03-27 19:55:53 +0800

    init commit

--- demo/eks/policy/script.sh
+++ demo/eks/policy/script.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 function updatecfosfirewalladdress {
+  echo updatecfosfirewalladdress IP=$IP
   curl -H "Content-Type: application/json" -X POST -d '{ "data": {"name": "'$IP'", "subnet": "'$IP' 255.255.255.255"}}' http://fos-deployment.default.svc.cluster.local/api/v2/cmdb/firewall/address
 }
 
 function updatecfosfirewalladdressgroup {
                 local memberlist="$1"
+                echo memberlist=$memberlist
                 if curl \
                   -H "Content-Type: application/json" \
                   -X PUT \
@@ -46,9 +48,21 @@ function createcfosfirewallpolicy {
 
 
 function getPodNet1Ips {
-  local namesapce="$1"
-  local label="$2"
-  kubectl get pods -n "$namespace" -l "$label" -o json | jq -r  '.items[].metadata.annotations."k8s.v1.cni.cncf.io/network-status" | fromjson | .[] | select(.interface == "net1") | .ips[]' | uniq | tr '\n' ' '
+  #local namesapce="$1"
+  #local label="$2"
+  kubectl get pods -n "$NAMESPACE" -l "$LABEL_SELECTOR" -o json | jq -r  '.items[].metadata.annotations."k8s.v1.cni.cncf.io/network-status" | fromjson | .[] | select(.interface == "net1") | .ips[]' | uniq | tr '\n' ' '
+}
+
+
+function createClientPod {
+  while true; do
+  if kubectl get pod clientpod 
+  then 
+  break
+  else 
+      kubectl run clientpod --image=praqma/network-multitool
+  fi
+  done
 }
 
 function updateCfos {
@@ -63,6 +77,7 @@ function updateCfos {
 
                     IP_LIST+=("$IP")
                     echo "New pod IP address detected: $IP, update cfos firewall address"
+                    echo IP=$IP
 
                     updatecfosfirewalladdress
 
@@ -71,11 +86,14 @@ function updateCfos {
 
                 done
               
-                memberlist="[$(echo "$MEMBER1" | sed 's/,$//')]"
-                echo $memberlist
-  
+               if [ -z "$IP" ]; then 
+               echo $IP is empty
+               else 
+               memberlist="[$(echo "$MEMBER1" | sed 's/,$//')]"
                 EXECLUDEIP="none"
+                echo call updatecfosfirewalladdressgroup $memberlist
                 updatecfosfirewalladdressgroup $memberlist
+               fi
                 
 }
 
@@ -99,11 +117,14 @@ done
 }
 
 # Set the namespace and deployment name
-NAMESPACE="default"
+#NAMESPACE="default"
+NAMESPACE=$NAMESPACE
 DEPLOYMENT_NAME="multitool01-deployment"
 
+
 # Set the label selector for the pods you want to watch
-LABEL_SELECTOR="app=multitool01"
+#LABEL_SELECTOR="app=multitool01"
+LABEL_SELECTOR=$LABEL_SELECTOR
 
 SRC_ADDR_GROUP=$(echo $NAMESPACE$LABEL_SELECTOR | sed 's/[^A-Za-z]//g')
 #SRC_ADDR_GROUP="cfossrc"
@@ -112,9 +133,12 @@ INTERVAL=10
 # Initialize an empty list to store the IP addresses
 
 POD_IPS=$(getPodNet1Ips $NAMESPACE  $LABEL_SELECTOR)
-#echo calling updateCfos with $POD_IPS
+
+if [ -z $POD_IPS ]; then 
+echo "no ip exist "
+else
 updateCfos
 createcfosfirewallpolicy
-
+fi
 watchPodandUpdateCfosFirwallAddressGrpforSelectedNamespaceandLabel
 
