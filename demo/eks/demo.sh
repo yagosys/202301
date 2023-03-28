3bf35e7dddbaa52d2482eae5fe21f50f259ac210
andy <yagosys@gmail.com>
2023-03-28 19:42:43 +0800

    init commit

--- demo/eks/demo.sh
+++ demo/eks/demo.sh
@@ -17,18 +17,34 @@ function restart_cfos_if_not_ready {
     done
 }
 
+function insert_snat_entry_if_not_exist {
+	node_list=$(kubectl get nodes -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
+	 for nodeName in $node_list; do
+            cfospod=`kubectl get pods -l app=fos --field-selector spec.nodeName=$nodeName |    cut -d ' ' -f 1 | tail -n -1`
+	    if kubectl  exec -it po/$cfospod -- iptables -t nat -C  fcn_nat -o eth0 -j MASQUERADE
+            then 
+		    echo "snat exist, do nothing"
+		    break
+	    else   
+		    echo "insert snat entry to eth0"
+		    kubectl exec -it po/$cfospod -- iptables -t nat -A fcn_nat -o eth0 -j MASQUERADE
+           fi
+	 done
+}
+
 function install_gatekeeperv3 {
     kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/master/deploy/gatekeeper.yaml
 }
 
 kubectl create -f multus-daemonset.yml
-kubectl create -f  nad_bridge_cn_cfosdefaultcni5_10_1_200.yaml
+#kubectl create -f  nad_bridge_cn_cfosdefaultcni5_10_1_200.yaml
 #kubectl create -f nad_bridge_cni_10_0_200_cfosdefauultcni5.yaml
+kubectl create -f nad_bridge_cn_cfosdefaultcni5_10_1_200_no_snat.yaml
 kubectl rollout status ds/kube-multus-ds -n kube-system
 sleep 10
 kubectl create -f dockersecret.yaml
 kubectl create -f fos_license.yaml
-kubectl create -f cfos_firewallpolicy.yaml
+#kubectl create -f cfos_firewallpolicy.yaml
 sleep 10
 kubectl create -f app.yaml
 kubectl rollout status deployment
@@ -39,8 +55,11 @@ kubectl create -f cfos.yaml
 echo "sleep 10"
 sleep 10
 kubectl rollout status ds/fos-deployment
-restart_cfos_if_not_ready
+#restart_cfos_if_not_ready
+insert_snat_entry_if_not_exist 
 echo "sleep 10"
+sleep 10
+kubectl create -f ./policy/watchandupdatcfospodip.yaml
 sleep 10
 kubectl get pod | grep multi | grep -v termin  | awk '{print $1}'  | while read line; do kubectl exec -t po/$line -- ping -c1 1.1.1.1 ; done
 
