31101c449a80243e03e0ac8ce82e4cc4b1e7e267
i <interbeing@appledeiMac.local>
2023-05-02 16:30:00 +0800

    init

--- demo/gke/updategkevm.sh
+++
@@ -1,5 +0,0 @@
-gcloud compute instances update-from-file gke-my-first-cluster-1-default-pool-426b2669-fwm8 \
-    --project cfos-384323 \
-    --zone us-west1-a \
-    --source=gke-my-first-cluster-1-default-pool-426b2669-fwm8.txt \
-    --most-disruptive-allowed-action=REFRESH
