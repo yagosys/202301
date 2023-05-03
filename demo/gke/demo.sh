a324669c55bb285e1899a6e5f2b0316aa59d7484
xpwang <xpwang@vitaomics.com>
2023-05-03 01:09:31 +0000

    init

--- demo/gke/demo.sh
+++ demo/gke/demo.sh
@@ -1,4 +1,8 @@
-echo $(date)
+file="$HOME/license/dockerinterbeing.yaml"
+[ -e $file ] && echo found dockerpullsecret $file || echo "$file  does not exist, exit" && 
+file="$HOME/license/fos_license.yaml"
+[ -e $file ] && echo found cfos license file $file || echo "$file  does not exist,exit" &&
+startdate=$(date)
 ./00_gcloud_env.sh && \
 ./00_create_network.sh && \
 ./01_gke.sh && \
@@ -14,7 +18,9 @@ echo $(date)
 ./11_cfos_ds_restart.sh && \
 ./12_ipstest.sh && \
 ./13_webftest.sh && \
-echo $(date)
+echo "-----------" 
+echo deploy start from $startdate to $(date)
 echo 'done'
+echo "-----------"
 echo 'do not forget delete resource created in this demo script use ./14_delcuster.sh && ./15_deleteNetwork.sh'
 
