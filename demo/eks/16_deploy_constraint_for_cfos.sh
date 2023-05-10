bf72b80145efd0c79e66209aed2d8f8722997c0b
xpwang <xpwang@vitaomics.com>
2023-05-10 03:28:10 +0000

    init

---
+++ demo/eks/16_deploy_constraint_for_cfos.sh
@@ -0,0 +1,29 @@
+filename="16_constraint_for_cfos.yml"
+cat << EOF >$filename
+apiVersion: constraints.gatekeeper.sh/v1beta1
+kind: K8sEgressNetworkPolicyToCfosUtmPolicy
+metadata:
+  name: cfosnetworkpolicy
+spec:
+  enforcementAction: deny
+  match:
+    kinds:
+      - apiGroups: ["networking.k8s.io"]
+        kinds: ["NetworkPolicy"]
+  parameters:
+    firewalladdressapiurl : "http://fos-deployment.default.svc.cluster.local/api/v2/cmdb/firewall/address"
+    firewallpolicyapiurl : "http://fos-deployment.default.svc.cluster.local/api/v2/cmdb/firewall/policy"
+    firewalladdressgrpapiurl: "http://fos-deployment.default.svc.cluster.local/api/v2/cmdb/firewall/addrgrp"
+    policyid : "200"
+    label: "cfosegressfirewallpolicy"
+    outgoingport: "eth0"
+    utmstatus: "enable"
+    ipsprofile: "default"
+    avprofile: "default"
+    sslsshprofile: "deep-inspection"
+    action: "permit"
+    srcintf: "any"
+    extraservice: "PING"
+EOF
+
+kubectl apply -f $filename && kubectl get k8segressnetworkpolicytocfosutmpolicy -o yaml
---
+++ demo/eks/18_redeploy_policy_manager.sh
@@ -0,0 +1,1 @@
+./08_create_policy_manager.sh 
