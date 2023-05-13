53304de038bf16d59eca0a8e09384dbee0ca139d
xpwang <xpwang@vitaomics.com>
2023-05-13 10:24:20 +0000

    init

---
+++ demo/gke/18_create_policy_manager.sh
@@ -0,0 +1,82 @@
+filename="18_cfospolicymanager.yml"
+policymanagerimage="interbeing/kubectl-cfos:latest"
+
+function wait_for_pod_ready {
+pod_name=$(kubectl get pods -l app=policy_manager -o jsonpath='{.items[0].metadata.name}')
+
+while true; do
+    pod_status=$(kubectl get pods $pod_name -o jsonpath='{.status.phase}')
+    if [[ $pod_status == "Running" ]]; then
+	kubectl get pod -l app=policy_manager
+        break
+    else
+        echo "Waiting for pod to be in Running state..."
+        sleep 5
+    fi
+done
+
+}
+
+cat << EOF > $filename
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: pod-reader
+
+---
+
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: pod-reader
+rules:
+- apiGroups: [""]
+  resources: ["pods"]
+  verbs: ["list", "get", "watch"]
+- apiGroups: [""]
+  resources: ["configmaps"]
+  verbs: ["list","get","watch","create"]
+- apiGroups: [""]
+  resources: ["nodes"]
+  verbs: ["list", "get", "watch"]
+- apiGroups: ["apps"]
+  resources: ["daemonsets"]
+  verbs: ["get", "list", "watch", "patch", "update"]
+- apiGroups: ["constraints.gatekeeper.sh"]
+  resources: ["k8segressnetworkpolicytocfosutmpolicy"]
+  verbs: ["list","get","watch"]
+    #- apiGroups: [""]
+    #  resources: ["pods/exec"]
+    #  verbs: ["create"]
+    #
+
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: pod-reader
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: pod-reader
+subjects:
+- kind: ServiceAccount
+  name: pod-reader
+  namespace: default
+
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: policymanager
+  labels: 
+    app: policy_manager
+spec:
+  serviceAccountName: pod-reader
+  containers:
+  - name: kubectl-container
+    image: $policymanagerimage
+EOF
+kubectl apply -f $filename  && wait_for_pod_ready
+
