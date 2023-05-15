6377cc04d62deccdedc636814bb31b0f26145dff
xpwang <xpwang@vitaomics.com>
2023-05-15 05:21:04 +0000

    init

--- demo/gke/variable.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-export networkName="gkenetwork1" 
-export subnetName="gkenode" 
-export ipcidrRange="10.0.0.0/24" 
-export firewallruleName="$networkName-allow-custom" 
-export firewallallowProtocol="all"
-export defaultClustername="my-first-cluster-1"
-export machineType="e2-standard-2"
-export num_nodes="1"
