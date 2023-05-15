bb98dd746306d672e0183f6f015be94896d58d13
xpwang <xpwang@vitaomics.com>
2023-05-15 05:20:15 +0000

    init

--- demo/gke/06_apply_license.sh
+++
@@ -1,5 +0,0 @@
-file="$HOME/license/dockerinterbeing.yaml"
-[ -e $file ] && kubectl create -f $file || echo "$file  does not exist"
-file="$HOME/license/fos_license.yaml"
-[ -e $file ] && kubectl create -f $file || echo "$file  does not exist"
-
--- demo/gke/20_pod_command.sh
+++
@@ -1,2 +0,0 @@
-command=$1
-kubectl get pod | grep multi | grep -v termin  | awk '{print $1}'  | while read line; do echo pod $line; kubectl exec -t po/$line -- $command ; done
