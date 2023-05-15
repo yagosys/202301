8045a138aa0389a3ec2bd3a7c77abde2ea31e431
xpwang <xpwang@vitaomics.com>
2023-05-15 05:19:18 +0000

    init

--- demo/gke/14_install_gatekeepv3.sh
+++
@@ -1,6 +0,0 @@
-kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/master/deploy/gatekeeper.yaml && \
-kubectl rollout status deployment/gatekeeper-audit -n gatekeeper-system &&  \
-kubectl rollout status deployment/gatekeeper-controller-manager  -n gatekeeper-system 
-
-
-
