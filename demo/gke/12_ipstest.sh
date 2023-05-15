70ee28c297170ebca4d1752687a932a479611a78
xpwang <xpwang@vitaomics.com>
2023-05-15 05:16:32 +0000

    init

--- demo/gke/12_ipstest.sh
+++
@@ -1,5 +0,0 @@
-url="www.hackthebox.eu"
-kubectl get pod | grep multi | grep -v termin | awk '{print $1}'  | while read line; do kubectl exec -t po/$line -- dig $url ; done && \
-kubectl get pod | grep multi | grep -v termin | awk '{print $1}'  | while read line; do kubectl exec -t po/$line -- ping -c 5  $url ; done && \
-kubectl get pod | grep multi | grep -v termin | awk '{print $1}'  | while read line; do kubectl exec -t po/$line --  curl --max-time 5  -k -H "User-Agent: () { :; }; /bin/ls" https://$url ; done
-kubectl get pod | grep fos | awk '{print $1}'  | while read line; do kubectl exec -t po/$line -- tail  /data/var/log/log/ips.0  ; done
