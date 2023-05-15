c40954a3ec051734a53760953a074182ea8e2e34
xpwang <xpwang@vitaomics.com>
2023-05-15 09:51:15 +0000

    init

--- demo/gke/cfos_multus/07_apply_license.sh
+++ demo/gke/cfos_multus/07_apply_license.sh
@@ -1,4 +1,4 @@
-file="$HOME/license/dockerinterbeing.yaml"
+file="$HOME/license/dockerpullsecret.yaml"
 [ -e $file ] && kubectl create -f $file || echo "$file  does not exist"
 file="$HOME/license/fos_license.yaml"
 [ -e $file ] && kubectl create -f $file || echo "$file  does not exist"
