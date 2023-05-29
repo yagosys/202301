3546d891cf08aa8043cb3fae88ed9dee1d9b59bd
yagosys <yagosys@gmail.com>
2023-05-29 13:46:36 +0800

    init

--- demo/kubevirt/run.sh
+++ demo/kubevirt/run.sh
@@ -27,6 +27,22 @@ wget -O ~/virtctl https://github.com/kubevirt/kubevirt/releases/download/${KUBEV
 chmod +x ~/virtctl
 sudo install ~/virtctl /usr/local/bin
 
+echo install krew
+
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
+echo export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+kubectl krew install virt
+
 echo "disable kvm"
 kubectl -n kubevirt patch kubevirt kubevirt --type=merge --patch '{"spec":{"configuration":{"developerConfiguration":{"useEmulation":true}}}}'
 
