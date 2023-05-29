9d23a79ab609d44043d4350374d0cef1b54872bb
yagosys <yagosys@gmail.com>
2023-05-29 21:11:54 +0800

    init

--- demo/kubevirt/vnc.sh
+++ demo/kubevirt/vnc.sh
@@ -1,10 +1,10 @@
-sudo apt update
-sudo apt install xfce4 xfce4-goodies
-sudo apt install tightvncserver
-sudo apt install xtightvncviewer
-sudo apt install virt-viewer
-sudo apt install terminator
-sudo apt install xterm
+sudo apt update -y 
+sudo apt install xfce4 xfce4-goodies -y 
+sudo apt install tightvncserver -y
+sudo apt install xtightvncviewer -y 
+sudo apt install virt-viewer -y
+sudo apt install terminator -y 
+sudo apt install xterm -y
 
 mkdir -p ~/.vnc
 touch  ~/.vnc/xstartup
