601a289c2f8ead7cdd08f9fe24d0502b9a09839a
yagosys <yagosys@gmail.com>
2023-08-01 21:04:19 +0000

    init

--- demo/gke/cfos_multus/doc/gen_doc.sh
+++ demo/gke/cfos_multus/doc/gen_doc.sh
@@ -2,9 +2,7 @@ filename="README.md"
 echo "README" > $filename
 files=$(ls -l ./../??_*.md | awk '{print $9}')
 for file in $files; do
-echo cat "$file" >> $filename
 echo -e "\n" >> $filename
 cat $file >> $filename
-echo "end of $file" >> $filename
 echo -e "\n" >> $filename
 done
