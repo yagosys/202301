8ef5e4f624e55576bc2f3c7ccf754b5a0a2866a3
yagosys <yagosys@gmail.com>
2023-08-02 03:51:27 +0000

    init

--- demo/gke/cfos_multus/00_a_gcloud_env.sh
+++ demo/gke/cfos_multus/00_a_gcloud_env.sh
@@ -1,14 +1,15 @@
 #/bin/bash -xe
-echo $networkName
-project="$1"
-region="$2"
-zone="$3"
+filename="00_a_gcloud_env.sh.gen.sh"
 
-[[ "$1" == "" ]] && project="cfos-384323"
-[[ "$2" == "" ]] && region="asia-east1"
-[[ "$3" == "" ]] && zone="asia-east1-a"
-gcloud config set project $project
-gcloud config set compute/region $region
-gcloud config set compute/zone $zone
+cat << EOF > $filename
+project=\$(gcloud config list --format="value(core.project)")
+export region="asia-east1"
+export zone="asia-east1-a"
+gcloud config set project \$project
+gcloud config set compute/region \$region
+gcloud config set compute/zone \$zone
 gcloud config list
+EOF
+chmod +x $filename
 
+./$filename
