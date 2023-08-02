4f1bb8efb5d014819d0ac56bbfc4542294c1190c
yagosys <yagosys@gmail.com>
2023-08-02 01:18:39 +0000

    init

--- demo/gke/cfos_multus/07_apply_license.sh
+++ demo/gke/cfos_multus/07_apply_license.sh
@@ -1,5 +1,26 @@
-file="$HOME/license/dockerpullsecret.yaml"
-[ -e $file ] && kubectl create -f $file || echo "$file  does not exist"
-file="$HOME/license/fos_license.yaml"
+[[ -z $cfos_license_input_file ]] && cfos_license_input_file="FGVMULTM23000044.lic"
+[[ -f $cfos_license_input_file ]] ||  echo $cfos_license_input_file does not exist
+mkdir -p $HOME/license
+file="$HOME/license/cfos_license.yaml"
+licensestring=$(sed '1d;$d' $cfos_license_input_file | tr -d '\n')
+cat <<EOF >$file
+apiVersion: v1
+kind: ConfigMap
+metadata:
+    name: fos-license
+    labels:
+        app: fos
+        category: license
+data:
+    license: |
+     -----BEGIN FGT VM LICENSE-----
+     $licensestring
+     -----END FGT VM LICENSE-----
+     
+EOF
+
+#file="$HOME/license/dockerpullsecret.yaml"
+#[ -e $file ] && kubectl create -f $file || echo "$file  does not exist"
+file="$HOME/license/cfos_license.yaml"
 [ -e $file ] && kubectl create -f $file || echo "$file  does not exist"
 
