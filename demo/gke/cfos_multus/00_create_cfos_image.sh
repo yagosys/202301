5c906d9b604dbc8b494c7546889c5bd0e94118ce
yagosys <yagosys@gmail.com>
2023-08-02 01:17:26 +0000

    init

--- demo/gke/cfos_multus/00_create_cfos_image.sh
+++ demo/gke/cfos_multus/00_create_cfos_image.sh
@@ -8,22 +8,3 @@ docker push gcr.io/$PROJECT_ID/fos:7231
 export cfos_image="gcr.io/$PROJECT_ID/fos:7231"
 echo $cfos_image
 
-[[ -z $cfos_license_input_file ]] && cfos_license_input_file="FGVMULTM23000044.lic"
-[[ -f $cfos_license_input_file ]] ||  echo $cfos_license_input_file does not exist
-licensestring=$(sed '1d;$d' $cfos_license_input_file | tr -d '\n')
-cat <<EOF >fos_license.yaml
-apiVersion: v1
-kind: ConfigMap
-metadata:
-    name: fos-license
-    labels:
-        app: fos
-        category: license
-data:
-    license: |
-     -----BEGIN FGT VM LICENSE-----
-     $licensestring
-     -----END FGT VM LICENSE-----
-     
-EOF
-ls -l fos_license.yaml
